6cdd1c521d4c864a60bf973b5d3f88152d2f5ca6
Giovane <giovaneaalmeida27@gmail.com>
2021-10-17 18:55:56 -0300

    feat: create InvalidDateError

---
+++ src/utils/errors/InvalidDateError.ts
@@ -0,0 +1,11 @@
+
+
+class InvalidDateError extends Error {
+  constructor() {
+    super()
+    this.name = 'InvalidDateError'
+    this.message = 'O campo prazo está incorreto, insira uma data no formato YYYY-MM-DD HH:mm:ss'
+  }
+}
+
+export default InvalidDateError
--- src/utils/errors/index.ts
+++ src/utils/errors/index.ts
@@ -1,9 +1,11 @@
 import InvalidParamError from './InvalidParamError'
 import MissingParamError from './MissingParamError'
 import ServerError from './ServerError'
+import InvalidDateError from './InvalidDateError'
 
 export {
   InvalidParamError,
   MissingParamError,
-  ServerError
+  ServerError,
+  InvalidDateError
 }
