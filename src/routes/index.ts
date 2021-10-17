eecc088a52be4da5fa937b1a212a391daaf3dec5
Giovane <giovaneaalmeida27@gmail.com>
2021-10-17 19:07:39 -0300

    feat(routes): add delete endpoint

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -10,6 +10,7 @@ const todoController = new TodoController()
 app.get('/tarefas', todoController.index)
 app.post('/tarefas', todoController.create)
 app.put('/tarefas/:identificador', todoController.update)
+app.delete('/tarefas/:identificador', todoController.delete)
 
 app.listen(3000, () => {
   console.log('-----------------Server running on port 3000-----------------')
