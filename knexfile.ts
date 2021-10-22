5c1234b5283e23ebbac03b580488b13fe01f5d25
Giovane <giovaneaalmeida27@gmail.com>
2021-10-22 13:19:15 -0300

    refact: troca banco para sqlite3

--- knexfile.ts
+++ knexfile.ts
@@ -3,13 +3,9 @@
 module.exports = {
 
   development: {
-    client: 'mysql',
+    client: 'sqlite3',
     connection: {
-      host: '127.0.0.1',
-      port: 3306,
-      user: 'root',
-      password: '',
-      database: 'todos_database'
+      filename: './database.sqlite'
     },
     migrations: {
       directory: './src/database/migrations'
